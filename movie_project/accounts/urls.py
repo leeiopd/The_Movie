a9e07957718094edbb0735f5d1b40c7cc9952711
leeiopd <leeiopd@hanmail.net>
2019-05-14 17:53:03 +0900

    accounts | subscribe, likes 기능& 페이지 구현 / 그 외 수정

--- movie_project/accounts/urls.py
+++ movie_project/accounts/urls.py
@@ -6,13 +6,18 @@ app_name = 'accounts'
 urlpatterns = [
     path('<int:user_id>/', views.userInfo, name='user_info'),
     path('signup/', views.signup, name='signup'),
-    path('update/info', views.updateProfile, name='updateProfile'),
-    path('update/genre', views.updateGenre, name='updateGenre'),
+    path('update/profile/', views.updateUser, name='updateUser'),
+    path('update/proflieTogenre', views.proflieTogenre, name='proflieTogenre'),
+    path('update/genre/', views.updateGenre, name='updateGenre'),
     path('signin/', views.signin, name='signin'),
     path('logout/', views.log_out, name='logout'),
     path('<int:user_id>/subscribe/', views.subscribe, name="subscribe"),
-    path('<int:user_id>/subscribeList', views.subscribeList, name='subscribeList'),
-    path('<int:user_id>/subscriberList', views.subscriberList, name='subscriberList'),
+    path('<int:user_id>/subscribeList/', views.subscribeList, name='subscribeList'),
+    path('<int:user_id>/subscriberList/', views.subscriberList, name='subscriberList'),
+    path('<int:user_id>/feed/', views.viewfeed, name='feed'),
+    path('<int:user_id>/likes/casts/', views.likeCasts, name='likeCasts'),
+    path('<int:user_id>/likes/directors/', views.likeDirectors, name='directors'),
+
     
 
 ]
--- movie_project/accounts/views.py
+++ movie_project/accounts/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Profile
-from movies.models import Genre
+from movies.models import Genre, Review
 from .forms import CreateForm, LoginForm, ProfileForm
 from django.contrib.auth import get_user_model, login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
 
 # Create your views here.
 
+@require_http_methods(['GET'])
 def userInfo(request, user_id):
     user_info = get_object_or_404(get_user_model(), pk=user_id)
     user = get_object_or_404(get_user_model(), pk=request.user.pk)
     context = {'user_info':user_info, 'user':user}
     return render(request, 'accounts/info.html', context)
 
-
+@require_http_methods(['GET', 'POST'])
 def signup(request):
     if request.user.is_authenticated:
         return redirect('movies:main')
@@ -26,26 +28,32 @@ def signup(request):
                 user = userform.save()
                 Profile.objects.create(user=user, point=0)
                 login(request, user)
-                return redirect('accounts:updateProfile')
+                return redirect('accounts:updateUser')
         else:
             userform = CreateForm()
         context = {'userform': userform}
         return render(request, 'accounts/signup.html', context)
         
 
+@require_http_methods(['GET', 'POST'])
 @login_required
-def updateProfile(request):
+def updateUser(request):
     if request.method == 'POST':
         userProfileform = ProfileForm(request.POST, instance=request.user.profile)
         if userProfileform.is_valid():
             userProfileform.save()
-            return redirect('accounts:profile')
+            return redirect('accounts:proflieTogenre')
     else:
         userProfileform = ProfileForm(instance=request.user.profile)
     context = {'userProfileform':userProfileform}
     return render(request, 'accounts/profile.html', context)
 
+@login_required
+def proflieTogenre(request):
+    return render(request, 'accounts/profileTogenre.html')
 
+
+@require_http_methods(['GET', 'POST'])
 @login_required
 def updateGenre(request):
     user = get_object_or_404(get_user_model(), pk=request.user.pk)
@@ -62,15 +70,15 @@ def updateGenre(request):
             like_genre.like_users.add(user)
 
         user_genres = user.like_genres.all()
-        print(user_genres)
         context = {'user_genres': user_genres}
-        return redirect('accounts:profile')
+        return redirect('accounts:user_info', user.pk)
     else:
         user_genres = user.like_genres.all()
         context = {'user_genres': user_genres}
         return render(request, 'accounts/genre.html', context)
 
 
+@require_http_methods(['GET', 'POST'])
 def signin(request):
     if request.user.is_authenticated:
         return redirect('movies:main')
@@ -85,11 +93,12 @@ def signin(request):
         context = {'userLoginform':userLoginform}
         return render(request, 'accounts/signin.html', context)
 
-
+@require_http_methods(['GET'])
 @login_required
 def log_out(request):
     logout(request)
     return redirect('accounts:signin')
+
 
 @login_required
 def subscribe(request, user_id):
@@ -106,6 +115,7 @@ def subscribe(request, user_id):
         return JsonResponse({'is_subscribe':is_subscribe, 'subscribe_count':target_user.subscribers.count()})
 
 
+
 @login_required
 def subscribeList(request, user_id):
     user = get_object_or_404(get_user_model(), pk=user_id)
@@ -120,3 +130,31 @@ def subscriberList(request, user_id):
     subscribers = user.subscribers.all()
     context = {'subscribers':subscribers}
     return render(request, 'accounts/subscribers.html', context)
+
+
+
+def viewfeed(request, user_id):
+    user = get_object_or_404(get_user_model(), pk=user_id)
+    # feeds = user.subscribe.prefetch_related('review_set')
+    subscribes = user.subscribe.values_list('id')
+    feeds = Review.objects.filter(user_id__in=subscribes).order_by('-id')
+    # feeds = []
+    # for user in allSubUser:
+    #     feeds.append(user.review_set.all())
+
+    context = {'feeds': feeds}
+    return render(request, 'accounts/feed.html', context)
+
+
+def likeCasts(request, user_id):
+    user = get_object_or_404(get_user_model(), pk=user_id)
+    casts = user.like_casts.all()
+    context = {'casts': casts}
+    return render(request, 'accounts/casts.html', context)
+
+
+def likeDirectors(request, user_id):
+    user = get_object_or_404(get_user_model(), pk=user_id)
+    directors = user.like_directors.all()
+    context = {'directors': directors}
+    return render(request, 'accounts/directors.html', context)
