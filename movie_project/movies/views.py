c8c8534ce06bc507ed0d311b1fede34c0d09d80a
dohbam <patlian@naver.com>
2019-05-14 16:52:50 +0900

    movies | comment CRUD(axios) 완성

--- movie_project/movies/views.py
+++ movie_project/movies/views.py
@@ -82,6 +82,7 @@ def update(request, movie_pk, review_pk):
 @require_POST
 @login_required
 def create_comment(request, movie_pk, review_pk):
+    print(request.POST)
     if request.is_ajax():
         review = get_object_or_404(Review, pk=review_pk)
         form = CommentForm(request.POST)
@@ -103,9 +104,10 @@ def create_comment(request, movie_pk, review_pk):
 
 @require_POST
 @login_required
-def delete_comment(request, movie_pk, review_pk, comment_pk):
+def delete_comment(request, comment_pk):
     if request.is_ajax():
         comment = get_object_or_404(Comment, pk=comment_pk)
+        movie_pk = comment.review.movie.pk
         if request.user == comment.user or request.user.is_superuser:
             comment.delete()
             return redirect('movies:detail', movie_pk)
@@ -117,12 +119,14 @@ def delete_comment(request, movie_pk, review_pk, comment_pk):
 
 @login_required
 @require_POST
-def update_comment(request, movie_pk, review_pk, comment_pk):
+def update_comment(request, comment_pk):
     if request.is_ajax():
         comment = get_object_or_404(Comment, pk=comment_pk)
+        movie_pk = comment.review.movie.pk
         if request.user == comment.user or request.user.is_superuser:
             form = CommentForm(request.POST, instance=comment)
             if form.is_valid():
+                print('valid')
                 comment = form.save(commit=False)
                 # comment.user = request.user
                 # review = get_object_or_404(Review, pk=review_pk)
@@ -131,11 +135,13 @@ def update_comment(request, movie_pk, review_pk, comment_pk):
                 data = {'userPk': comment.user.pk,
                         'username': comment.user.username,
                         'content': comment.content,
-                        'moviePk' : movie_pk,
-                        'reviewPk': review_pk,
+                        'moviePk' : comment.review.movie.pk,
+                        'reviewPk': comment.review.pk,
                         'commentPk': comment.pk,
                         }
                 return JsonResponse(data)
+            print('hi')
+            return JsonResponse({'error': str(form)})
         else:
             messages.add_message(request, messages.WARNING, 'You are not a writer.')
             return redirect('movies:detail', movie_pk)
