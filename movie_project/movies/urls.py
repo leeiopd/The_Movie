44434714a05d9766940835dd7f41358ebcf0b7fc
dohbam <patlian@naver.com>
2019-05-14 13:35:41 +0900

    movies | comment CRD, base.html

--- movie_project/movies/urls.py
+++ movie_project/movies/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
     path('<int:movie_pk>/<int:review_pk>/update/', views.update, name='update'),
     # path('<int:movie_pk>/<int:review_pk>/detail/', views.review_detail, name='review_detail'),
     path('<int:movie_pk>/<int:review_pk>/create/', views.create_comment, name='create_comment'),
+    path('<int:movie_pk>/<int:review_pk>/<int:comment_pk>/delete/', views.delete_comment, name='delete_comment'),
+    path('<int:movie_pk>/<int:review_pk>/<int:comment_pk>/update/', views.update_comment, name='update_comment'),
+
 ]
--- movie_project/movies/views.py
+++ movie_project/movies/views.py
@@ -3,27 +3,35 @@ from django.db.models import Avg
 from django.views.decorators.http import require_http_methods, require_POST
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseBadRequest
 from .models import Movie, Review, Comment
 from .forms import ReviewForm, CommentForm
 
 # Create your views here.
 def main(request):
     movies = Movie.objects.annotate(score_avg=Avg('review__score')).order_by('-score_avg')[:20]
-    return render(request, 'movies/main.html', {'movies': movies})
+    # 리뷰 작성 여부 확인
+    user_movies = request.user.review_set.values_list('movie__id', flat=True)
+    context = {'movies': movies, 'user_movies': user_movies}
+    return render(request, 'movies/main.html', context)
 
 def detail(request, movie_pk):
     movie = Movie.objects.annotate(score_avg=Avg('review__score')).get(pk=movie_pk)
+    reviewed = False
+    user_movies = request.user.review_set.values_list('movie__id', flat=True)
+    if movie_pk in user_movies:
+        reviewed = True
     form = CommentForm()
-    context = {'movie': movie, 'form': form}
+    context = {'movie': movie, 'form': form, 'reviewed': reviewed}
     return render(request, 'movies/detail.html', context)
 
 @login_required
 @require_http_methods(['GET', 'POST'])
 def create(request, movie_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)
-    # 이미 리뷰를 작성한 적 있다면
-    if movie in request.user.movie_set.all():
+    # 리뷰 작성 여부 확인
+    user_movies = request.user.review_set.values_list('movie__id', flat=True)
+    if movie.pk in user_movies:
         messages.add_message(request, messages.WARNING, '이미 리뷰를 작성하셨습니다.')
         return redirect('movies:detail', movie_pk)
     else:
@@ -37,7 +45,7 @@ def create(request, movie_pk):
                 request.user.profile.point += 100
                 request.user.profile.save()
                 print(request.user.profile.point)
-                return redirect('movies:main')
+                return redirect('movies:detail', movie_pk)
         else:
             form = ReviewForm()
         context = {'form': form}
@@ -74,18 +82,63 @@ def update(request, movie_pk, review_pk):
 @require_POST
 @login_required
 def create_comment(request, movie_pk, review_pk):
-    review = get_object_or_404(Review, pk=review_pk)
-    form = CommentForm(request.POST)
-    if form.is_valid():
-        comment = form.save(commit=False)
-        comment.user = request.user
-        comment.review = review
-        comment.save()
-        data = {'userPk': comment.user.pk,
-                'username': comment.user.username,
-                'content': comment.content,
-                'reviewPk': comment.review.pk,
-                'commentPk': comment.pk
-                }
-        return JsonResponse(data)
+    if request.is_ajax():
+        review = get_object_or_404(Review, pk=review_pk)
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.review = review
+            comment.save()
+            data = {'userPk': comment.user.pk,
+                    'username': comment.user.username,
+                    'content': comment.content,
+                    'moviePk' : movie_pk,
+                    'reviewPk': review_pk,
+                    'commentPk': comment.pk,
+                    }
+            return JsonResponse(data)
+    else:
+        return HttpResponseBadRequest
+
+@require_POST
+@login_required
+def delete_comment(request, movie_pk, review_pk, comment_pk):
+    if request.is_ajax():
+        comment = get_object_or_404(Comment, pk=comment_pk)
+        if request.user == comment.user or request.user.is_superuser:
+            comment.delete()
+            return redirect('movies:detail', movie_pk)
+        else:
+            messages.add_message(request, messages.WARNING, 'You are not a writer.')
+            return redirect('movies:detail', movie_pk)
+    else:
+        return HttpResponseBadRequest
+
+@login_required
+@require_POST
+def update_comment(request, movie_pk, review_pk, comment_pk):
+    if request.is_ajax():
+        comment = get_object_or_404(Comment, pk=comment_pk)
+        if request.user == comment.user or request.user.is_superuser:
+            form = CommentForm(request.POST, instance=comment)
+            if form.is_valid():
+                comment = form.save(commit=False)
+                # comment.user = request.user
+                # review = get_object_or_404(Review, pk=review_pk)
+                # comment.review = review
+                comment.save()
+                data = {'userPk': comment.user.pk,
+                        'username': comment.user.username,
+                        'content': comment.content,
+                        'moviePk' : movie_pk,
+                        'reviewPk': review_pk,
+                        'commentPk': comment.pk,
+                        }
+                return JsonResponse(data)
+        else:
+            messages.add_message(request, messages.WARNING, 'You are not a writer.')
+            return redirect('movies:detail', movie_pk)
+    else:
+        return HttpResponseBadRequest
 
